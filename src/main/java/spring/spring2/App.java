d9a7da6d6436c9b5774e3d5805d14f1b6444fa18
wyj <wang.yujia@navercorp.com>
2016-05-04 15:29:17 +0800

    first commit

---
+++ src/main/java/spring/spring2/App.java
@@ -0,0 +1,13 @@
+package spring.spring2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldddd!" );
+    }
+}
---
+++ src/main/java/spring/spring2/Application.java
@@ -0,0 +1,28 @@
+package spring.spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World!";
+    }
+    
+    @RequestMapping("/world")
+    @ResponseBody
+    public String index2() {
+        return "Hello World!!!!!!!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
